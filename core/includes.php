14435c7949a0979b32e0a2de2b039d7096085f29
Kéwan Bernier <mail@kewan.fr>
2023-02-21 21:34:11 +0100

    hotfix V1.5.0 (21-02-23)

--- core/includes.php
+++ core/includes.php
@@ -1,5 +1,4 @@
 <?php
-require 'Session.php';
 require 'Form.php';
 require 'functions.php';
 require 'Router.php';
@@ -10,6 +9,9 @@ require ROOT . DS . 'config' . DS . 'conf.php';
 require 'Request.php';
 require 'Controller.php';
 require 'Model.php';
+
+require 'Session.php';
+
 require 'Dispatcher.php';
 
 ?>
