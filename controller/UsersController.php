fa407c5e9ef0c898f3a1abe7dd55d8b300c6b77f
Kéwan Bernier <mail@kewan.fr>
2023-02-21 17:50:50 +0100

    feat(form): Add register form validation #7

--- controller/UsersController.php
+++ controller/UsersController.php
@@ -18,15 +18,14 @@ class UsersController extends Controller
       if($user && password_verify($this->request->data->password, $user->password)){
         unset($user->password);
         $this->Session->write('User', $user);
-        $this->Session->setFlash('Connexion Réussie !', 'success');
+        $this->Session->setFlash('Connexion Réussie !', 'success', 1);
         $this->redirect('admin');
       }else{
         $this->Session->write('User', null);
         $this->Session->setFlash('Identifiant ou mot de passe incorrect', 'danger');
       }
       $this->request->data->password = '';
-    }
-    if($this->Session->isLogged()){
+    }else if($this->Session->isLogged()){
       if($this->Session->user('role') == 'admin'){
         $this->redirect('admin');
       }else {
@@ -42,26 +41,39 @@ class UsersController extends Controller
     }
     if($this->request->data)
     {
-      $data = $this->request->data;
-      $this->loadModel('User');
-      $user = $this->User->findFirst(array(
-        'conditionsor' => array(
-          'login' => $data->login,
-          'email' => $data->email,
-        )
-      ));
-      if($user){
-        $this->Session->setFlash('Identifiant déjà utilisé', 'danger');
+      $errors = $this->Form->validation($this->request->data);
+      if(!empty($errors)){
+        $this->Session->setFlash("Veuillez corriger les erreurs suivantes :", "danger", null, $errors);
+        $this->set('errors', $errors);
       }else{
-        $data->password = password_hash($data->password, PASSWORD_DEFAULT);
-        $data->role = 'admin';
-        $userId = $this->User->save($data);
-        $data->id = $userId;
-        
-        $this->Session->write('User', $data);
-        $this->Session->setFlash('Inscription Réussie !', 'success');
+        $data = $this->request->data;
+        $this->loadModel('User');
+        $user = $this->User->findFirst(array(
+          'conditionsor' => array(
+            'login' => $data->login,
+            'email' => $data->email,
+          )
+        ));
+        if($user){
+          $this->Session->setFlash('Identifiant ou adresse email déjà utilisé', 'danger');
+        }else{
+          $password = password_hash($data->password, PASSWORD_DEFAULT);
+          $role = 'admin';
+          $newUser = (object) array(
+            'login' => $data->login,
+            'email' => $data->email,
+            'password' => $password,
+            'role' => $role,
+          );
+          $userId = $this->User->save($newUser);
+          $newUser->id = $userId;
+          unset($newUser->password);
+          
+          $this->Session->write('User', $newUser);
+          $this->Session->setFlash('Inscription Réussie !', 'success', 1);
+          $this->redirect('admin');
+        }
       }
-      $this->redirect('admin');
     }else if($this->Session->isLogged()){
       if($this->Session->user('role') == 'admin'){
         $this->redirect('admin');
